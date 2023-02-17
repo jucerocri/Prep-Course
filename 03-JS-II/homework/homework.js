b0123585abae075eb6b3eedcf2057bffeeef65a1
jucerocri <jucerocri@hotmail.com>
2023-02-17 14:33:25 -0500

    Actualizacion homework 03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -130,8 +130,6 @@ function fizzBuzz(numero) {
  
   if (numero % 15 == 0){
     return "fizzbuzz";
-  }else if (numero % 15 == 0){
-    return "fizzbuzz";
   }else if (numero % 3 == 0){
     return "fizz";
   }else if (numero % 5 == 0){
